5899ae502ac7cf3d97feaa34652f3c03d259e296
niuniuanran <irene.anran.niu@gmail.com>
2020-03-17 20:48:50 +1300

    flat design

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    backgroundColor: theme => ({
+      'trip-orange': "#ff4200",
+      'trip-blue-light': "#C9E2E9",
+      'trip-blue': "#2F8CAB"
+    })
+  },
+  variants: {},
+  plugins: [],
+}
