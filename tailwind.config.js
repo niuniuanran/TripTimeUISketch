da236b1d3fd096b7c23f2448ce5b6c395c7b4713
niuniuanran <irene.anran.niu@gmail.com>
2020-03-17 23:18:53 +1300

    flat page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+
+
 module.exports = {
   theme: {
-    backgroundColor: theme => ({
+    colors: theme => ({
       'trip-orange': "#ff4200",
       'trip-blue-light': "#C9E2E9",
-      'trip-blue': "#2F8CAB"
-    })
+      'trip-blue': "#2F8CAB",
+      'white':"#fff",
+      "black": "#000",
+      "trip-gray": "#777",
+      "trip-gray-light":"#ddd",
+      "trip-gray-bright": "#f5f5f5"
+    }),
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
   },
   variants: {},
   plugins: [],
