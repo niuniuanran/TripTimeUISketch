2e29c5c8ac56fd4751e16abfb320056b232ba362
niuniuanran <irene.anran.niu@gmail.com>
2020-03-18 15:55:26 +1300

    migrated item styles into modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
-
-
 module.exports = {
-  theme: {
-    colors: theme => ({
-      'trip-orange': "#ff4200",
-      'trip-blue-light': "#C9E2E9",
-      'trip-blue': "#2F8CAB",
-      'white':"#fff",
-      "black": "#000",
-      "trip-gray": "#777",
-      "trip-gray-light":"#ddd",
-      "trip-gray-bright": "#f5f5f5"
-    }),
-    screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-    }
-  },
-  variants: {},
-  plugins: [],
+    theme: {
+        colors: {
+            'trip-orange': "#ff4200",
+            'trip-blue-light': "#C9E2E9",
+            'trip-blue': "#2F8CAB",
+            'white': "#fff",
+            "black": "#000",
+            "trip-gray": "#777",
+            "trip-gray-light": "#ddd",
+            "trip-gray-bright": "#f5f5f5"
+        },
+        inset: {
+            'off-bar': '4rem',
+        },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+        },
+
+
+    },
+    variants: {},
+    plugins: [],
 }
